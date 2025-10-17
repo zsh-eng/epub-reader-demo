6b6a4f39b94b8dff654490fe0e69898dd7e3dc99
zsheng <zsheng.dev@gmail.com>
2025-10-17 13:10:52 +0800

    setup

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
