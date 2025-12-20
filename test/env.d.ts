9d91f620fe575d3e7884a65b618fc763ca9ffe1e
zsheng <zsheng.dev@gmail.com>
2025-12-20 12:25:39 +0800

    Set up migrations

--- test/env.d.ts
+++ test/env.d.ts
@@ -1,4 +1,6 @@
 declare module "cloudflare:test" {
   // ProvidedEnv controls the type of `import("cloudflare:test").env`
-  interface ProvidedEnv extends Env {}
+  interface ProvidedEnv extends Env {
+    TEST_MIGRATIONS: D1Migration[]; // Defined in `vitest.config.mts`
+  }
 }
