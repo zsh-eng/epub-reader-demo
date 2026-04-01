02010f8e6389019734ced1a16efc93cb2097b9be
zsheng <zsheng.dev@gmail.com>
2026-04-01 22:56:06 +0800

    Load fonts directly instead of google fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+
+import "@fontsource/eb-garamond/400.css";
+import "@fontsource/eb-garamond/500.css";
+import "@fontsource/eb-garamond/600.css";
+import "@fontsource/eb-garamond/400-italic.css";
+
+import "@fontsource/lora/400.css";
+import "@fontsource/lora/500.css";
+import "@fontsource/lora/600.css";
+import "@fontsource/lora/400-italic.css";
+
+import "@fontsource/jetbrains-mono/400.css";
+
 import "./index.css";
 import App from "./App.tsx";
 
